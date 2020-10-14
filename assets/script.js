8e25555a02b723c89953500ebb53626bec51bf85
Matty Party <matthewpewewardy@gmail.com>
2020-10-14 17:36:41 -0500

    adding font awesome link

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
+var cityName = "Wichita";
 var apikey = "e29b1c89c8da9ec620dfeea74ecf210c";
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=wichita,ks,usa&APPID=e29b1c89c8da9ec620dfeea74ecf210c";
+var queryUrlCurrentForecast =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  cityName +
+  "&appid=" +
+  apikey;
 
 $.ajax({
-  url: queryURL,
+  url: queryUrlCurrentForecast,
   method: "GET",
 }).then(function (response) {
-  //   console.log(response);
+  console.log(response);
   //   console.log(response.main.temp);
-  currentTempFah = (response.main.temp - 273.15) * (9 / 5) + 32;
-  console.log(currentTempFah);
-  Math.round(parseInt(currentTempFah));
-  console.log(currentTempFah);
 });
